287efd72ca9eb65275dd9459af0b0e170f9c5f45
Matthew Amundsen <ctrlaltdisease@gmail.com>
2015-10-21 17:32:40 +1300

    Attempted image additions
    Updated gitignore to remove images that were accidentally added. Added
    image tags to display pokemon. Added better form for pokemon page.

--- resources/views/pokemon/index.blade.php
+++ resources/views/pokemon/index.blade.php
@@ -9,8 +9,9 @@
 			@foreach($pokemon as $pokemon)
 				<li>
 					<h3><a href="{{ route('pokemon.show', $pokemon->id) }}">{{ $pokemon->name}}</a></h3>
+					<img src="images\pokemon\00{{ $pokemon->id }}.png">
 				</li>
 			@endforeach
 		</ul>
-	</div>	
+	</div>
 @endsection
--- resources/views/pokemon/show.blade.php
+++ resources/views/pokemon/show.blade.php
@@ -7,6 +7,7 @@
 	  <div class="container">
 		<h1>Pokemon</h1>
 		<h2>{{ $pokemon->name }}</h2>
+		<img src="images\pokemon\00{{ $pokemon->id }}.png">
 		@foreach ($comments as $comment)
 			<p>{{ $comment->comment }}</p>
 		@endforeach
@@ -14,10 +15,26 @@
 	</div>
 
 	<h4>Comment on {{ $pokemon->name }}</h4>
-	<form>
-		<label for="comment">Comment</label>
-		<textarea id="comment"name="comment"></textarea>
-		<button>Add Comment</button>
+	
+	<form method="POST" action="./?page=comment.create" class="form-horizontal" enctype="multipart/form-data">
+		<input type="hidden" name="pokemon_id" value="<?= $pokemon->id ?>">
+
+		<div class="form-group">
+			<label for="comment" class="col-sm-4 col-md-2 control-label">Comment</label>
+			<div class="col-sm-8 col-md-10">
+				<textarea id="comment" class="form-control" name="comment" rows="5"></textarea>
+				<div class="help-block"></div>
+			</div>
+		</div>
+
+		<div class="form-group">
+			<div class="col-sm-offset-4 col-sm-10 col-md-offset-2 col-md-10">
+				<button class="btn btn-default">
+					<span class="glyphicon glyphicon-comment"></span> Add Comment
+				</button>
+			</div>
+		</div>
+
 	</form>
 
 @endsection
