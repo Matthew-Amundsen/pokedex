3703033f3f6e079c419c073b08a726bb58df6d0b
Matthew-Amundsen <ctrlaltdisease@gmail.com>
2015-11-09 19:52:58 +1300

    add visual stlying to pokemon images in the index and search views.

--- resources/views/pokemon/index.blade.php
+++ resources/views/pokemon/index.blade.php
@@ -8,11 +8,11 @@
 		<div class="row">
 			@foreach($pokemon as $pokemon)
 				<a href="{{ route('pokemon.show', $pokemon->id) }}">
-					<div class="col-sm-6 col-md-4 text-center pokemon-index">
-						<img src="..\images\pokemon\{{ $pokemon->id }}.png" width="200">
-						<h3>{{ $pokemon->id}} | {{ $pokemon->name}}</h3>
-					</div>
-				</a>
+				<div class="col-sm-6 col-md-4 pokemon-index">
+					<img src="..\images\pokemon\{{ $pokemon->id }}.png" width="270">
+					<div class="pokemon-number"><p>{{ $pokemon->id}}</p></div><div class="pokemon-name"><p>{{ $pokemon->name}}</p></div>
+				</div>
+			</a>
 			@endforeach
 		</div>
 
--- resources/views/pokemon/search.blade.php
+++ resources/views/pokemon/search.blade.php
@@ -7,9 +7,9 @@
 		
 		@foreach($result as $pokemon)
 			<a href="{{ route('pokemon.show', $pokemon->id) }}">
-				<div class="col-sm-6 col-md-4 text-center pokemon-index">
-					{{-- <img src="..\images\pokemon\{{ $pokemon->id }}.png" width="200"> --}}
-					<h3 class="pokemon-number"><p>{{ $pokemon->id}}</p></h3><h3 class="pokemon-name"><p>{{ $pokemon->name}}</p></h3>
+				<div class="col-sm-6 col-md-4 pokemon-index">
+					<img src="..\images\pokemon\{{ $pokemon->id }}.png" width="270">
+					<div class="pokemon-number"><p>{{ $pokemon->id}}</p></div><div class="pokemon-name"><p>{{ $pokemon->name}}</p></div>
 				</div>
 			</a>	
 		@endforeach
